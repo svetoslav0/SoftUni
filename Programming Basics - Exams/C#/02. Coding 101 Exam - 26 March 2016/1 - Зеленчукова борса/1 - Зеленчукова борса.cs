e31424430a7eb7816f0c6324c0b76b2386ac85b3
svetoslav0 <sssvetoslavvv@gmail.com>
2018-03-27 15:01:20 +0300

    Uploaded first exercise from 26 march 2016 exam

---
+++ Programming Basics - Exams/C#/02. Coding 101 Exam - 26 March 2016/1 - Зеленчукова борса/1 - Зеленчукова борса.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1___Зеленчукова_борса
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vegPrice = double.Parse(Console.ReadLine());
+            double fruitPrice = double.Parse(Console.ReadLine());
+            double totalVegs = double.Parse(Console.ReadLine());
+            double totalFruits = double.Parse(Console.ReadLine());
+            double totalVegsPrice = vegPrice * totalVegs;
+            double totalFruitPrice = fruitPrice * totalFruits;
+            double totalPrice = (totalFruitPrice + totalVegsPrice) / 1.94;
+            Console.WriteLine(totalPrice);
+            
+            
+        }
+    }
+}
