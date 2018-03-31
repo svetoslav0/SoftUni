6c7773c14eb6aabe04806258dd86b6f427049735
Svetoslav Nedelchev <sssvetoslavvv@gmail.com>
2018-03-31 22:11:56 +0300

    Uploaded solution of Frist exercise from 04 March PF exam

---
+++ Programming Fundamentals - Exams/C#/04 March 2018/01. Padawan Equipment/01. Padawan Equipment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01._Padawan_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            double students = double.Parse(Console.ReadLine());
+            double sabrePrice = double.Parse(Console.ReadLine());
+            double robePrice = double.Parse(Console.ReadLine());
+            double beltPrice = double.Parse(Console.ReadLine());
+            double totalSabers = Math.Ceiling(students * 1.1);
+            double totalSabrePrice = sabrePrice * totalSabers;
+            double totalRobePrice = robePrice * students;
+            double freeBelts = Math.Floor(students / 6);
+            double totalBeltPrice = beltPrice * (students - freeBelts);
+            double totalCost = totalSabrePrice + totalRobePrice + totalBeltPrice;
+
+            if (totalCost <= money)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalCost:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {totalCost - money:f2}lv more.");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals - Exams/C#/04 March 2018/02. Kamino Factory/02. Kamino Factory.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Kamino_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            byte[] bestDNA = new byte[n - 1];
+            int bestIndex = 0;
+            int bestLength = 0;
+            int bestSum = 0;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Clone them!")
+                {
+                    break;
+                }
+                int[] currentSequence = input.Split('!',StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                int bestCurrentIndex = 0;
+                int bestCurrentLength = 0;
+                int bestCurrentSum = 0;
+                foreach (var item in currentSequence)
+                {
+                    if (item == 1)
+                    {
+                        bestCurrentLength = 1;
+                    }
+                }
+
+                for (int i = 0; i < currentSequence.Length - 1; i++)
+                {
+                    if (currentSequence[i] == 1 && currentSequence[i + 1] == 1)
+                    {
+                        bestCurrentIndex = i;
+                        bestCurrentLength++;
+                    }
+                    else
+                    {
+                        bestCurrentLength = 1;
+                    }
+                    if (currentSequence[i] == 1)
+                    {
+                        bestCurrentSum++;
+                    }
+                }
+                if (bestCurrentIndex < bestIndex)
+                {
+                    bestIndex = bestCurrentIndex;
+                }
+                if (bestCurrentLength > bestLength)
+                {
+                    bestLength = bestCurrentLength;
+                }
+                if (bestCurrentSum > bestSum)
+                {
+                    bestSum = bestCurrentSum;
+                }
+            }
+            Console.WriteLine(bestIndex);
+            Console.WriteLine(bestLength);
+            Console.WriteLine(bestSum);
+
+        }
+    }
+}
